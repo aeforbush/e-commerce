bfa3a063e6b70f08242557cec72d63d68cf7018d
aeforbush <87487655+aeforbush@users.noreply.github.com>
2021-10-18 10:23:28 -0600

    created JOINS in index.js

--- models/index.js
+++ models/index.js
@@ -4,13 +4,26 @@ const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
+// This is where the tables JOIN
 // Products belongsTo Category
+Product.belongsTo(Category, {
+
+})
 
 // Categories have many Products
+Category.hasMany(Product, {
+
+})
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+
+})
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  
+})
 
 module.exports = {
   Product,
